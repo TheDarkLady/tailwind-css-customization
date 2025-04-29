0c178908723d0d33c2a3b4a236d664a853a2baa0
TheDarkLady <lakshmichaithanya13@gmail.com>
2025-04-29 16:32:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+      <div className=" flex flex-col items-center justify-center min-h-svh">
+        <Button className="bg-[#7269fe]">Click me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
